2bc8bdf61fef438fc6d69d0e351cd98a5f6ccf9c
vlad <vlad@gmail.com>
2018-06-12 09:45:07 +0300

    aded the router + the relevant templates

--- frontend/admin/index.js
+++ frontend/admin/index.js
@@ -1,22 +1,36 @@
 const admin = angular.module("admin", ["ngRoute"]);
 
-admin.config(function($routeProvider){
+admin.config(function($routeProvider) {
   $routeProvider
-    .when("/",{
+    .when("/", {
       templateUrl: "templates/main.htm",
-      controller: "testCtrl"
+      controller: "mainCtrl"
     })
-    .when("/settings",{
-      templateUrl: "templates/settings.htm"
+    .when("/categories", {
+      templateUrl: "templates/categories.htm",
+      controller: "categoriesCtrl"
     })
-    .when("/categories",{
-      templateUrl: "templates/categories.htm"
+    .when("/products", {
+      templateUrl: "templates/products.htm",
+      controller: "productsCtrl"
+    })
+    .when("/orders", {
+      templateUrl: "templates/orders.htm",
+      controller: "settingsCtrl"
+    })
+    .when("/users", {
+      templateUrl: "templates/users.htm",
+      controller: "usersCtrl"
+    })
+    .when("/settings", {
+      templateUrl: "templates/settings.htm",
+      controller: "settingsCtrl"
     })
     .otherwise({
-      template: "<h1> da </h1>"
+      template: "<h1> otherwise route was hit. this route does not exist.. add it to your config. </h1>"
     });
 });
 
-admin.controller("testCtrl", function($scope){
-  $scope.name = "yes"; 
+admin.controller("testCtrl", function($scope) {
+  $scope.name = "yes";
 });
