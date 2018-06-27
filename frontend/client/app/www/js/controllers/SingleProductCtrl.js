7180ac78e2fbcb90dc0164a740071b4f05a5e4a6
PssThemes <pssthemestest@gmail.com>
2018-06-27 10:26:04 +0300

    i need to stop and work on the login functionality since i need the client to do stuff.. like add reviews..replies and so on..

--- frontend/client/app/www/js/controllers/SingleProductCtrl.js
+++ frontend/client/app/www/js/controllers/SingleProductCtrl.js
@@ -4,19 +4,35 @@ export default function SingleProductCtrl($scope, $stateParams, $firebaseArray, 
 
   const productId = $stateParams.productId;
 
-  const productRef = db.ref("products/" + productId);
+  // PRODUCT
+  $scope.product = $firebaseObject(db.ref("products/" + productId));
 
-  $scope.product = $firebaseObject(productRef);
-  console.log($scope.product);
+
+  // PRODUCT REVIEWS
+  $scope.reviews = $firebaseArray(db.ref("products/" + productId + "/reviews"));
+
+  $scope.saveReview = (message, value) => {
+    const review = {
+      clientId : "fake client id",
+      date: Date.now(),
+      message: message,
+      replies: [],
+      value : value
+    }
+    $scope.reviews.$add(review)
+  }
+
   $scope.longDescription = "The Apple Watch, with its inbuilt speaker and microphone, gives you the freedom to call your friends directly from your wrist. This splash-resistant smartwatch features an S1P dual-core processor for a fast and smooth performance. It has a digital crown, along with a heart rate sensor, gyroscope, ambient light sensor and an accelerometer. Its Rose Gold aluminium case and Midnight Blue strap further add to its appeal.";
 
-  $scope.showReviews = true;
 
+  $scope.showReviews = true;
   $scope.toggleReviews = () => {
     $scope.showReviews = !$scope.showReviews;
   }
 
+
   $scope.adminImage = $firebaseObject(db.ref("adminImage"));
+
 
   $scope.clients = {
     "-LFvnFfrOYynaicGdxAH" : {
@@ -41,7 +57,7 @@ export default function SingleProductCtrl($scope, $stateParams, $firebaseArray, 
   }
 
   // TODO ensure that only the logged in user has the right to reply to it's own messages.
-  
+
   $scope.reply = (reviewId, message) => {
     $scope.product.reviews[reviewId].replies.push({
       who: "client",
--- resources/client/tasks.md
+++ resources/client/tasks.md
@@ -12,3 +12,28 @@ Next tasks:
 DONE: clicking on a category should open that exact cat.. pass the category id to the individual category page. This means the list of products is queried from firebase based on that specific category.
 
 add fake 3 fake products to firebase and link them to this category: -LFWaisUPF_XssTJE10r
+
+
+
+## Finishing product page.
+
+
+Reviews for One product.
+- bind the reviews list inside the dom.
+- add ability to create review.
+- use a dummy user for now.
+
+
+REPLIES for ONE REVIEW
+- construct a replies reference in firebase.
+- bind that to the dom - show the replies out..
+
+
+create and send reply.
+- add a button for sending the reply
+- bind a reply on click even to that button
+- use replies reference to $add() on it.
+
+
+security.
+- ensure that the user logged in is the only one able to reply to stuff.
