01af8655392a3a7552baf6154545ebb5918baf33
AionDev <developer.andrasesc.ionut@gmail.com>
2018-07-23 09:54:40 +0300

    aded a new way and more granular way of extracting data we need.

--- backend/shopify.js
+++ backend/shopify.js
@@ -14,8 +14,21 @@ Shared.loadCustomProductsFromFirebase(SHOPNAME)
   const settings = result[0];
 
   // dictionary of { customCategoryId: String , externalCategoryId: String }
-  const mappingsBetweenInternalAndExternalCategories = result[1];
+  const pairsOfIntenralAndExternalCateories =  result[1];
+  const dictOfOneIntenralToManyExternalCategoriesIds = pairsOfIntenralAndExternalCateories.reduce((acc, pair) => {
+    // if this is the first element.. then create a internalCategoryId key and assign [] such that we can have a .push() function.
+    // if is not .. then use the existing value of the key..
+    // push the extenralCategoryId into the
+    // use the previous value.. or use an empty array such that we can have a .push() function on it
+    // in case this is the first element
+    acc[pair.internalCategoryId] = acc[pair.internalCategoryId]  || [];
+    acc[pair.internalCategoryId].push(pair.externalCategoryId);
+    return acc;
 
+  }, {})
+  const dictOfOneExternalToManyIntenralCategoriesIds = pairsOfIntenralAndExternalCateories.reduce(( acc, pair ) => {
+    return acc[]
+  })
   // in firebase products share a common interface, no matter form what shop they come from.
   // we call that interface and custom data asociated with it an intenral product.
   const internalProducts = result[2] || {};
@@ -28,7 +41,17 @@ Shared.loadCustomProductsFromFirebase(SHOPNAME)
   // load all externalProducts from shopify..
   const promisedExternalProducts = loadAllShopifyProducts(settings);
   promisedExternalProducts
-    .then(externalProducts => {
+    .then(allExternalProducts => {
+
+      // relevant is about taking into consideration that not all products form the shop are included in our system.
+      // we exclude them based on their asociated external categories..
+      // by checking if at least 1 internal category is linked to any of this product external categories.
+
+      const relevantExternalProducts = allExternalProducts.filter();
+      const normalizedExternalProducts = relevantExternalProducts.map(normalizeShopifyProduct);
+      const idsOfExternalProducts = normalizedExternalProducts.map(p => p.externalId);
+
+
 
     })
     .catch(error => {
@@ -48,4 +71,8 @@ function loadAllShopifyProducts(){
   return new Promise((resolve, reject) => {
     resolve("some crap");
   });
+}
+
+function normalizeShopifyProduct(rawProduct){
+  return "normalized product."
 }
