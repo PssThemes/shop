bf145412c719a9c826c3b99227833d77c7000b61
AionDev <developer.andrasesc.ionut@gmail.com>
2018-08-13 06:43:16 +0300

    done some datastructures

--- backend/shared/Data.elm
+++ backend/shared/Data.elm
@@ -1,23 +1,95 @@
 module Data exposing (..)
 
-import Json.Decode as JD
+-- import Json.Decode as JD
+
+import Set exposing (Set)
 
 
 type ExternalProductId
     = ExternalProductId String
 
 
+type ExternalCatId
+    = ExternalCatId String
+
+
+type InternalCatId
+    = InternalCatId String
+
+
+type InternalProductId
+    = InternalProductId String
+
+
+type ShopName
+    = Shopify
+    | Prestashop
+
+
+type alias CategoryName =
+    String
+
+
+
+-- type ShopifyProduct =
+--   ShopifyProduct {
+--     name: String
+--
+-- }
+
+
 type alias InternalProduct =
-    { externalProductId : ExternalProductId
+    { selfId : InternalProductId
+
+    -- external identification.
+    , shopName : ShopName
+    , externalId : ExternalProductId
     , name : String
-    , mainProductImage : Maybe String
+    , short_description : String
     , price : Float
+
+    --
+    , externalCatIds : Set ExternalCatId
+    , internalCatIds : Set InternalCatId
+
+    --
+    , mainImage : Maybe String
     , media : List String
+
+    --
+    , isHidden : Bool
+    , howManyTimesWasOrdered : Int
+    }
+
+
+type alias InternalCategory =
+    { selfId : InternalCatId
+    , name : String
+    , shopify : List ( ExternalCatId, CategoryName )
+    , prestashop : List ( ExternalCatId, CategoryName )
+    }
+
+
+type alias Settings =
+    { shopify :
+        { apiKey : String
+        , apiSecret : String
+        , shopName : String
+        }
+    , prestashop :
+        { apiKey : String
+        }
     }
 
 
 type alias NormalizedProduct =
-    {}
+    { externalId : ExternalProductId
+    , name : String
+    , mainImage : Maybe String
+    , price : Float
+    , description : String
+    , media : List String
+    }
 
 
 
--- backend/shared/Ports.elm
+++ backend/shared/Ports.elm
@@ -1,7 +1,24 @@
 port module Ports exposing (..)
 
+import Data exposing (..)
+import Dict exposing (Dict)
+import Json.Decode as JD
+
+
+-- Outgoing ports
+
+
+port finish : () -> Cmd msg
+
+
+
+-- Incoming ports
+
 
 port start : (() -> msg) -> Sub msg
 
 
-port finish : () -> Cmd msg
+port received_settings : (Settings -> msg) -> Sub msg
+
+
+port received_internalCategories : (JD.Value -> msg) -> Sub msg
