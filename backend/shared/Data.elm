49f24596fd642ac449477426a2e916204ecb259f
AionDev <developer.andrasesc.ionut@gmail.com>
2018-08-23 15:36:04 +0300

    left the code on building the trasnformer form raw into normalized product.

--- backend/shared/Data.elm
+++ backend/shared/Data.elm
@@ -256,13 +256,19 @@ type alias RawPrestashopProduct =
 
 type alias NormalizedProduct =
     { externalId : ExternalProductId
+    , internalCatIds : EverySet InternalCatId
+
+    --
     , name : String
-    , mainImage : Maybe String
     , price : Float
+
+    --
     , short_description : String
     , description : String
+
+    --
+    , mainImage : Maybe String
     , media : List String
-    , internalCatIds : EverySet InternalCatId
     }
 
 
@@ -319,138 +325,10 @@ rawPrestashopProductDecoder =
         |> JDP.requiredAt [ "associations", "categories" ] (JD.list (JD.field "id" JD.string |> JD.map (\id -> { id = id })))
         |> JDP.requiredAt [ "associations", "images" ] (JD.list (JD.field "id" JD.string |> JD.map (\id -> { id = id })))
 
-
-
--- internalProductDecoder : JD.Decoder InternalProduct
--- internalProductDecoder =
---     JDP.decode
---         (\selfId shopName externalId name short_description price externalCatIds internalCatIds mainImage media isHidden howManyTimesWasOrdered ->
---             { selfId = InternalProductId selfId
---             , shopName = shopName
---             , externalId = ExternalProductId externalId
---             , name = name
---             , short_description = short_description
---             , price = price |> String.toFloat |> Result.withDefault 0
---             , externalCatIds = externalCatIds |> List.map ExternalCatId |> EverySet.fromList
---             , internalCatIds = internalCatIds |> List.map InternalCatId |> EverySet.fromList
---             , mainImage = mainImage
---             , media = media
---             , isHidden = isHidden
---             , howManyTimesWasOrdered = howManyTimesWasOrdered
---             }
---         )
---         |> JDP.required "selfId" (JD.string)
---         |> JDP.required "shopName" shopNameDecoder
---         |> JDP.required "externalId" (JD.string)
---         |> JDP.required "name" JD.string
---         |> JDP.required "short_description" JD.string
---         |> JDP.required "price" JD.string
---         |> JDP.required "externalCatIds" (JD.list JD.string)
---         |> JDP.required "internalCatIds" (JD.list JD.string)
---         |> JDP.optional "mainImage" (JD.string |> JD.map (\x -> Just x)) Nothing
---         |> JDP.required "media" (JD.list JD.string)
---         |> JDP.required "isHidden" JD.bool
---         |> JDP.required "howManyTimesWasOrdered" JD.int
--- normalizedProductDecoder : JD.Decoder NormalizedProduct
--- normalizedProductDecoder =
---     JD.oneOf [ shopifyProductDecoder, prestashopProductDecoder ]
--- shopifyProductDecoder : JD.Decoder NormalizedProduct
--- shopifyProductDecoder =
---     let
---         getShopifyShortDescription : String -> String
---         getShopifyShortDescription longDescription =
---             -- TODO: create a proper function for  getting the short description out of the long descripotion
---             -- since shopify does not have the notion of short_description by default.
---             String.left 300 longDescription
---     in
---         JDP.decode
---             (\id title maybe_mainImgSrc variants body_html images ->
---                 { externalId = ExternalProductId (toString id)
---                 , name = title
---                 , mainImage = maybe_mainImgSrc
---                 , price =
---                     variants
---                         |> List.head
---                         |> Maybe.map
---                             (\string ->
---                                 string
---                                     |> String.toFloat
---                                     |> Result.withDefault 0
---                             )
---                         |> Maybe.withDefault 0
---                 , short_description = getShopifyShortDescription body_html
---                 , description = body_html
---                 , media = []
---                 }
---             )
---             |> JDP.required "id" JD.int
---             |> JDP.required "title" JD.string
---             |> JDP.optionalAt [ "image", "src" ] (JD.string |> JD.map Just) Nothing
---             |> JDP.required "variants" (JD.list (JD.field "price" JD.string))
---             |> JDP.required "body_html" JD.string
---             |> JDP.required "images" (JD.list (JD.field "src" JD.string))
--- normalizedProductsDecoder : JD.Decoder (EveryDict ExternalProductId NormalizedProduct)
--- normalizedProductsDecoder =
---     JD.list normalizedProductDecoder
---         |> JD.map
---             (\list ->
---                 list
---                     |> List.foldl
---                         (\normalizedProduct acc ->
---                             EveryDict.insert normalizedProduct.externalId normalizedProduct acc
---                         )
---                         EveryDict.empty
---             )
--- prestashopProductDecoder : JD.Decoder NormalizedProduct
--- prestashopProductDecoder =
---     JD.fail "not implemented"
 -----------------------------------------------------------------
--- Delete, Create Update..
+-- Transformer constructs
 -----------------------------------------------------------------
--- // Encoders
--- newlyCreatedProductEncoder : ShopName -> NormalizedProduct -> JE.Value
--- newlyCreatedProductEncoder shopName normalizedProduct =
---     [ "shopName" => (shopName |> shopNameEncoder)
---     , "externalId" => (normalizedProduct.externalId |> (\(ExternalProductId id) -> id) |> JE.string)
---     , "name" => (normalizedProduct.name |> JE.string)
---     , "short_description" => (normalizedProduct.short_description |> JE.string)
---     , "price" => (normalizedProduct.price |> JE.float)
---     , "externalCatIds" => ([] |> JE.list)
---     , "internalCatIds" => ([] |> JE.list)
---
---     -- TODO: check what is better.. a null for main image or an empty string? since null in firbase means the filed does not exist.
---     , "mainImage" => (normalizedProduct.mainImage |> Maybe.map JE.string |> Maybe.withDefault JE.null)
---     , "media" => (normalizedProduct.media |> List.map JE.string |> JE.list)
---     , "isHidden" => (False |> JE.bool)
---     , "howManyTimesWasOrdered" => (0 |> JE.int)
---     ]
---         |> JE.object
---
--- updatableProductDataEncoder : NormalizedProduct -> EverySet ExternalCatId -> JE.Value
--- updatableProductDataEncoder normalizedProduct externalCatIds =
---     [ "externalId"
---         => (normalizedProduct.externalId
---                 |> (\(ExternalProductId externalId) ->
---                         JE.string externalId
---                    )
---            )
---     , "name" => JE.string normalizedProduct.name
---     , normalizedProduct.mainImage
---         |> Maybe.map JE.string
---         |> Maybe.withDefault JE.null
---         |> (,) "mainImage"
---     , "price" => (JE.float normalizedProduct.price)
---     , "description" => (JE.string normalizedProduct.description)
---     , "media"
---         => (normalizedProduct.media
---                 |> List.map JE.string
---                 |> JE.list
---            )
---     , "externalCatIds"
---         => (externalCatIds
---                 |> EverySet.map (\(ExternalCatId id) -> JE.string id)
---                 |> EverySet.toList
---                 |> JE.list
---            )
---     ]
---         |> JE.object
+
+transformRawShopifyProduct: RawShopifyProduct -> List ExternalCatId ->  NormalizedProduct
+transformRawShopifyProduct rawProduct externalCats =
+  
